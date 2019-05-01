edb17c1ef99407393c617c5aa944f25f96399a29
Ashish Garg <mragarg@gmail.com>
2019-05-01 09:34:22 -0400

    Using routes for different pages. Use .bind for non-arrow functions

--- src/App.js
+++ src/App.js
@@ -2,25 +2,73 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './Home';
+import About from './About';
+import Cats from './Cats';
+
+import {
+  Link, 
+  Route
+} from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       counter: 0
+    }
+    // I definitely have access to the instance
+    // I will use .bind to "lock in" the value of `this`
+    this._incrementCount = this._incrementCount.bind(this);
+    // Functions are callable objects.
+    // As an object, they can have methods.
+    // One of their methods is .bind
+    // .bind returns a new copy of the function, 
+    // with the value of `this` locked in .
+  }
+  
+  render() {
+    return(
+      <div className="App">
+      <h1>{this.state.counter}</h1>
+      <button onClick={this._incrementCount}>increment</button>
+      <button onClick={this._incrementCountArrow}>incrementArrow</button>
+        {/* Link is what you use instead of an anchor */}
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/cats">Cats</Link>
+
+        {/* <Home /> */}
+        <Route exact path="/" component={Home} />
+        {/* <About /> */}
+        <Route path="/about" component={About} />
+        {/* <Cats /> */}
+        <Route path="/cats" component={Cats} />
+
+      </div>
+    )
+  }
+
+  // normal method
+  _incrementCount() {
+    console.log("incrementCount was called");
+    console.log(this);
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  // Arrow functions give you "auto .bind"
+  _incrementCountArrow = () => {
+    console.log("incrementCountArrow was called");
+    console.log(this);
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+
 }
 
 export default App;
---
+++ src/Cats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cats(props) {
+    return (
+        <div>
+            <h1>😸</h1>
+        </div>
+    );
+}
+
+export default Cats;
